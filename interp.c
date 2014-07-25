bdb11b6d8d25d22794a125ec31dc8bdea75e9411
benhardy <benhardy+github@gmail.com>
2014-07-25 01:51:41 -0700

    added variable assignment and recall, fix various bugs

--- interp.c
+++ interp.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "opcodes.h"
 #include "vm.h"
@@ -15,6 +16,9 @@
 #define TOKEN_MULT          0x34
 #define TOKEN_DIV           0x35
 #define TOKEN_MOD           0x36
+
+#define TOKEN_EQUALS        0x60
+#define TOKEN_WORD          0x70
 
 #define TOKEN_OPEN_PAREN    0x80
 #define TOKEN_CLOSE_PAREN   0x81
@@ -31,6 +35,11 @@
 #define log_trace(...) ;
 #endif
 #define log_warn(...) fprintf(stderr,  __VA_ARGS__)
+
+int code[4096];
+int data[4096];
+int code_size = 0;
+int data_size = 0;
 
 typedef struct _Token {
   int kind;
@@ -57,8 +66,10 @@ void free_token_list(Token*head) {
 }
 
 char* token_to_string(Token*token) {
-  if (token->kind== TOKEN_NUMBER && token->token_table_index >= 0)
+  bool isString = (token->kind== TOKEN_NUMBER || token->kind == TOKEN_WORD);
+  if (isString && token->token_table_index >= 0) {
     return &token_table[token->token_table_index];
+  }
   switch(token->kind) {
     case TOKEN_PLUS: return "+";
     case TOKEN_MINUS: return "-";
@@ -89,6 +100,37 @@ void delete_buffer(Buffer* buffer) {
   free(buffer);
 }
 
+// have symbol table in a dumb linked list to begin with
+typedef struct _Symbol {
+  int32_t data_offset;
+  char *name;
+  struct _Symbol *next;
+} Symbol;
+
+Symbol * head_symbol = NULL;
+
+Symbol * find_symbol(char*name) {
+  Symbol *current = head_symbol;
+  while(current) {
+    log_trace("comparing %s to %s\n", name, current->name);
+    if (strcmp(name, current->name) == 0)
+      return current;
+    current=current->next;
+  }
+  return NULL;
+}
+
+Symbol * create_symbol(char*name) {
+  Symbol *current = (Symbol*)malloc(sizeof(Symbol));
+  current->next = head_symbol;
+  current->name = malloc(strlen(name)+1);
+  strcpy(current->name, name);
+  current->data_offset = data_size++;
+  head_symbol = current;
+  log_trace("head symbol %s @%d\n", head_symbol->name, head_symbol->data_offset);
+  return current;
+}
+
 
 Token* scan_input(Buffer *buffer) {
   Token *last = NULL;
@@ -111,6 +153,15 @@ Token* scan_input(Buffer *buffer) {
       token_table[token_table_index++] = '\0';
       current_token = new_token(TOKEN_NUMBER, token_start, NULL);
     }
+    else if (isalpha(ch)) {
+      do {
+        token_table[token_table_index++] = ch;
+        ch = buffer->content[buf_pos++];
+      } while(isalnum(ch));
+      buf_pos--; // spit that last character back out
+      token_table[token_table_index++] = '\0';
+      current_token = new_token(TOKEN_WORD, token_start, NULL);
+    }
     else {
       switch(ch) {
         case '(': kind = TOKEN_OPEN_PAREN; break;
@@ -120,6 +171,7 @@ Token* scan_input(Buffer *buffer) {
         case '*': kind = TOKEN_MULT; break;
         case '/': kind = TOKEN_DIV; break;
         case '%': kind = TOKEN_MOD; break;
+        case '=': kind = TOKEN_EQUALS; break;
         default:  kind = 0;
       }
       if (kind) {
@@ -171,12 +223,13 @@ AST_Node * new_branch_node() {
   return node;
 }
 
+AST_Node * parse_assignment(Token** tokens);
 AST_Node * parse_addables(Token** tokens);
 AST_Node * parse_multipliables(Token** tokens);
 AST_Node * parse_term(Token** tokens);
 AST_Node * parse_unary_minus(Token** tokens);
 
-void write_instructions(AST_Node* tree, int32_t*code);
+void write_instructions(AST_Node* tree);
 
 AST_Node * parse_term(Token** tokens) {
   log_trace("parse_term starts &tokens=%08lx\n", tokens);
@@ -194,14 +247,22 @@ AST_Node * parse_term(Token** tokens) {
     }
     current = current->next;
     *tokens = current;
-    if (child->leaf || !child->right)
-      return child;
-    node = new_branch_node();
-    node->left = child;
-    return node;
+    return child;
   }
   if (current->kind == TOKEN_NUMBER) {
     log_trace("parse_term found a number %s\n", token_to_string(current));
+    node = new_leaf_node(current);
+    current = current->next;
+    *tokens = current; // we consume this token, update pointer
+    return node;
+  }
+  if (current->kind == TOKEN_WORD) {
+    Symbol*sym = find_symbol(token_to_string(current));
+    if (!sym) {
+      fprintf(stderr, "Unknown symbol %s\n", token_to_string(current));
+      return NULL;
+    }
+    log_trace("parse_term found known symbol %s\n", token_to_string(current));
     node = new_leaf_node(current);
     current = current->next;
     *tokens = current; // we consume this token, update pointer
@@ -237,14 +298,14 @@ AST_Node * parse_unary_minus(Token** tokens) {
 }
 
 AST_Node * parse_multipliables(Token** tokens) {
-  log_trace("parse starts &tokens=%08lx\n", tokens);
+  log_trace("parse_multipliables starts &tokens=%08lx\n", tokens);
   Token *current = *tokens;
   AST_Node * lhs = parse_unary_minus(&current);
   if (!lhs) {
     return NULL;
   }
   while (current && (current->kind == TOKEN_MULT || current->kind == TOKEN_DIV || current->kind == TOKEN_MOD)) {
-    log_trace("parse: found connecting operator %s\n", token_to_string(current));
+    log_trace("parse_multipliables: found connecting operator %s\n", token_to_string(current));
     Token * operator = current;
     current = current->next;
     AST_Node * rhs = parse_unary_minus(&current);
@@ -264,14 +325,14 @@ AST_Node * parse_multipliables(Token** tokens) {
 
 
 AST_Node * parse_addables(Token** tokens) {
-  log_trace("parse starts &tokens=%08lx\n", tokens);
+  log_trace("parse_addables starts &tokens=%08lx\n", tokens);
   Token *current = *tokens;
   AST_Node * lhs = parse_multipliables(&current);
   if (!lhs) {
     return NULL;
   }
   while (current && (current->kind == TOKEN_PLUS || current->kind == TOKEN_MINUS)) {
-    log_trace("parse: found connecting operator %s\n", token_to_string(current));
+    log_trace("parse_addables: found connecting operator %s\n", token_to_string(current));
     Token * operator = current;
     current = current->next;
     AST_Node * rhs = parse_multipliables(&current);
@@ -289,8 +350,46 @@ AST_Node * parse_addables(Token** tokens) {
   return lhs;
 }
 
+AST_Node * parse_assignment(Token** tokens) {
+  log_trace("parse_assignment starts &tokens=%08lx\n", tokens);
+  Token *word = *tokens;
+  if (!word) {
+    return NULL;
+  }
+  if (word->kind != TOKEN_WORD) {
+    return parse_addables(tokens);
+  }
+  log_trace("parse_assignment found a word: =%s\n", word);
+  Token * maybeEquals = word->next;
+  if (maybeEquals == NULL || maybeEquals->kind != TOKEN_EQUALS) {
+    return parse_addables(tokens);
+  }
+  log_trace("parse_assignment found =\n");
+  // we have an assignment here.
+  Token*current = maybeEquals->next;
+  log_trace("parse_assignment going for RHS =\n");
+  AST_Node * rhs = parse_addables(&current);
+  if (!rhs) {
+    log_trace("parse_assignment bailing on RHS =\n");
+    return NULL;
+  }
+  if (current) {
+    log_trace("parse: found assignment %s\n", token_to_string(current));
+  } else {
+    log_trace("at end\n");
+  }
+  AST_Node *lhs = new_leaf_node(word);
+
+  AST_Node *parent = new_branch_node();
+  parent->left = lhs;
+  parent->right = rhs;
+  parent->operator = maybeEquals;
+  *tokens=current;
+  return parent;
+}
+
 AST_Node* begin_parsing(Token*head) {
-  return parse_addables(&head);
+  return parse_assignment(&head);
 }
 
 void free_tree(AST_Node* root) {
@@ -318,42 +417,71 @@ void print_postfix(AST_Node* tree) {
   }
 }
 
-
-void write_instructions_rec(AST_Node* tree, int32_t*code, int* code_pos) {
+void write_instructions_rec(AST_Node* tree) {
   if (tree->leaf) {
-    int value = atoi(token_to_string(tree->token));
-    if (tree->apply_unary_minus)
-      value = -value;
-    fprintf(stdout, "%04x %10s[%02x] #0x%04x\n", *code_pos, "PUSH", I_PUSH, value);
-    code[(*code_pos)++] = I_PUSH;
-    code[(*code_pos)++] = value;
-  } else {
-    write_instructions_rec(tree->left, code, code_pos);
-    if (tree->right) {
-      write_instructions_rec(tree->right, code, code_pos);
-      int op =0;
-      switch(tree->operator->kind) {
-        case TOKEN_MULT: op = I_MUL; break;
-        case TOKEN_DIV: op = I_DIV; break;
-        case TOKEN_MOD: op = I_MOD; break;
-        case TOKEN_PLUS: op = I_ADD; break;
-        case TOKEN_MINUS: op = I_SUB; break;
-      }
-      fprintf(stdout, "%04x %10s[%02x] \n", *code_pos, instructions[op], op);
-      code[(*code_pos)++] = op;
+    if (tree->token->kind == TOKEN_WORD) {
+      Symbol *sym = find_symbol(token_to_string(tree->token));
+      int32_t offset = sym->data_offset;
+      fprintf(stdout, "%04x %10s[%02x] #0x%04x\n", code_size, "LOADPUSH", I_LOADPUSH, offset);
+      code[code_size++] = I_LOADPUSH;
+      code[code_size++] = offset;
+    } else {
+      int value = atoi(token_to_string(tree->token));
+      if (tree->apply_unary_minus)
+        value = -value;
+      fprintf(stdout, "%04x %10s[%02x] #0x%04x\n", code_size, "PUSH", I_PUSH, value);
+      code[code_size++] = I_PUSH;
+      code[code_size++] = value;
     }
-    if (tree->apply_unary_minus) {
-      fprintf(stdout, "%04x %10s[%02x] \n", *code_pos, instructions[I_NEG], I_NEG);
-      code[(*code_pos)++] = I_NEG;
+  }
+  else {
+    Token* op = tree->operator;
+    if (op->kind == TOKEN_EQUALS) {
+      log_trace("makin assignment\n");
+      char * name = &token_table[tree->left->token->token_table_index];
+      log_trace("name = %s\n", name);
+      Symbol *sym = find_symbol(name);
+      log_trace("sym = %08lx\n", sym);
+      if (!sym) {
+        sym = create_symbol(name);
+        log_trace("created sym = %08lx\n", sym);
+      } else {
+        log_trace("found existing sym %s\n", sym->name);
+      }
+      write_instructions_rec(tree->right);
+      int32_t offset = sym->data_offset;
+      fprintf(stdout, "%04x %10s[%02x] #0x%04x\n", code_size, "POPSTORE", I_POPSTORE, offset);
+      code[code_size++] = I_POPSTORE;
+      code[code_size++] = offset;
+    }
+    else {
+      write_instructions_rec(tree->left);
+      if (tree->right) {
+        write_instructions_rec(tree->right);
+        int op =0;
+        switch(tree->operator->kind) {
+          case TOKEN_MULT: op = I_MUL; break;
+          case TOKEN_DIV: op = I_DIV; break;
+          case TOKEN_MOD: op = I_MOD; break;
+          case TOKEN_PLUS: op = I_ADD; break;
+          case TOKEN_MINUS: op = I_SUB; break;
+        }
+        fprintf(stdout, "%04x %10s[%02x] \n", code_size, instructions[op], op);
+        code[code_size++] = op;
+      }
+      if (tree->apply_unary_minus) {
+        fprintf(stdout, "%04x %10s[%02x] \n", code_size, instructions[I_NEG], I_NEG);
+        code[code_size++] = I_NEG;
+      }
     }
   }
 }
 
-void write_instructions(AST_Node* tree, int32_t*code) {
-  int code_pos = 0;
-  write_instructions_rec(tree, code, &code_pos);
-  fprintf(stdout, "%04x %10s[%02x] \n", code_pos, instructions[I_STOP], I_STOP);
-  code[code_pos++] = I_STOP;
+void write_instructions(AST_Node* tree) {
+  code_size = 0;
+  write_instructions_rec(tree);
+  fprintf(stdout, "%04x %10s[%02x] \n", code_size, instructions[I_STOP], I_STOP);
+  code[code_size++] = I_STOP;
 }
 
 void dump_tokens(Token*token_list) {
@@ -366,8 +494,6 @@ void dump_tokens(Token*token_list) {
 
 #define CODE_SIZE 4096
 #define DATA_SIZE 128
-int code[4096];
-int data[4096];
 
 int main(int argc, char**args) {
   bool keep_going = true;
@@ -391,7 +517,7 @@ int main(int argc, char**args) {
     } else {
       print_postfix(root);
       puts("\n");
-      write_instructions(root, code);
+      write_instructions(root);
       free_tree(root);
       init(code, CODE_SIZE, data, DATA_SIZE);
       execute(true);
